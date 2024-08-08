d67c2fd73800904dc0b09834266d6438b5fd747c
pickleMorty <avatarbliz@gmail.com>
2024-08-08 11:31:14 +0600

    init

---
+++ src/types/ton.ts
@@ -0,0 +1,24 @@
+type WalletAccount = {
+  chain: string;
+  publicKey: string;
+  address: string;
+};
+
+type WalletDevice = {
+  appName: string;
+  appVersion: string;
+  maxProtocolVersion: number;
+  platform: string;
+  features: Array<{ name: string } | string>;
+};
+
+export type TonWallet = {
+  account: WalletAccount;
+  device: WalletDevice;
+  name: string;
+  appName: string;
+  imageUrl?: string;
+  aboutUrl?: string;
+  openMethod?: string;
+};
+  

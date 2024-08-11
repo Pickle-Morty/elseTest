c542df0ce468cbfdb191079ab64132bcfbf601a9
ulik <ulik.magic@gmail.com>
2024-08-11 20:42:40 +0600

    fix gapg

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -5,7 +5,7 @@ import { LogoIcon } from '@/assets/icons'
 
 export const WelcomePage: FC = () => (
 	<div className='bg-white h-full min-h-screen pb-5'>
-		<div className='px-4 pt-[73px] flex flex-col justify-between min-h-screen'>
+		<div className='px-4 pt-[73px] flex flex-col justify-between gap-40 min-h-screen'>
 			<div className='justify-start text-black font-sf-pro text-center'>
 				<h1 className='text-title-1 font-medium mb-4'>Else People</h1>
 				<LogoIcon className='w-[146px] h-[146px] mx-auto mb-8' />
